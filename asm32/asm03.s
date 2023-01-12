aa747d18d6af078a8d708af36379015cb617ed9a
Marietteth78 <99668021+Marietteth78@users.noreply.github.com>
2023-01-12 16:40:16 +0100

    Update asm03.s

--- asm32/asm03.s
+++ asm32/asm03.s
@@ -1,12 +1,18 @@
 bits 32
 
 section .data
-	message db '1337', 4
+	
+	
+	message db '1337',10
+	messagelen: equ $-message
+	
+	nombre db '42'
+	nombrelen: equ $-nombre
 	
 	str: times 5 db 0,0  ; Allocate buffer of 5 bytes
 	strlen: equ $ - str
 
-	output: db 'Entrez 42 pour return 1',0x0a,10
+	output db 'Entrez 42 pour return 1',0x0a,10
 	outputlen: equ $-output
 	
 section .text
@@ -26,19 +32,36 @@ _start :
 	mov edx, strlen
 	int 80h
 	
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, str
-	mov edx, strlen
-	int 80h
+	lea esi, [nombre]
+    	lea edi, [str]
+    	mov ecx, nombrelen
+	
+	rep cmpsb         ; comparison of ECX number of bytes
+    	mov eax, 4        ; does not modify flags
+    	mov ebx, 1
+    	
+	je _equal
+	jne _nonequal
+	
+_equal:
 	
 	mov eax, 4
 	mov ebx, 1
 	mov ecx, message
-	mov edx, 13+1
+	mov edx, messagelen
 	int 80h
 	
-
-	mov eax, 1
+	jmp exit1
+	
+_nonequal:
+	jmp exit0
+	
+	
+	
+exit0:	mov eax, 1
 	mov ebx, 0
+	int 80h
+
+exit1:	mov eax, 1
+	mov ebx, 1
 	int 80h
