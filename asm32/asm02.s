653e6f3e665cededa51a1037068dcd4156dd3c9f
Marietteth78 <99668021+Marietteth78@users.noreply.github.com>
2023-01-12 14:28:01 +0100

    Update asm02.s

--- asm32/asm02.s
+++ asm32/asm02.s
@@ -1,7 +1,7 @@
 bits 32
 
 section .data
-	message db '1337', 4
+	message db '1337', 10
 	
 section .text
 	global _start
@@ -11,9 +11,10 @@ _start :
 	mov eax, 4
 	mov ebx, 1
 	mov ecx, message
-	mov edx, 13+1
+	mov edx, 4+1
 	int 80h
 	
 	mov eax, 1
 	mov ebx, 0
 	int 80h
+
