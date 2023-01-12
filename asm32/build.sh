4006d3c35da36ef3e0606ba1844a8ad4daaba0f6
Marietteth78 <99668021+Marietteth78@users.noreply.github.com>
2023-01-12 17:45:46 +0100

    Update build.sh

--- asm32/build.sh
+++ asm32/build.sh
@@ -1,3 +1,25 @@
+#!/bin/sh
 
-nasm -f elf32 -o $1.o $1.s
-ld -m elf_i386 -o $1 $1.o
+nasm -f elf32 asm01.s -o asm01.o
+ld asm01.o -o asm01 -m elf_i386
+rm asm01.o
+
+nasm -f elf32 asm02.s -o asm02.o
+ld asm02.o -o asm02 -m elf_i386
+rm asm02.o
+
+nasm -f elf32 asm03.s -o asm03.o
+ld asm03.o -o asm03 -m elf_i386
+rm asm03.o
+
+nasm -f elf32 asm04.s -o asm04.o
+ld asm04.o -o asm04 -m elf_i386
+rm asm04.o
+
+nasm -f elf32 asm05.s -o asm05.o
+ld asm05.o -o asm05 -m elf_i386
+rm asm05.o
+
+nasm -f elf32 asm06.s -o asm06.o
+ld asm06.o -o asm06 -m elf_i386
+rm asm06.o
